666ba904bb02d25c6ce4b01acb310088a2277cb4
Nithin Devarajan <nithin.devarajan@ad.infosys.com>
2020-08-05 16:38:43 +0530

    Test repo commit

--- src/main/java/com/qa/ApplicationSpecifics/ProjectSpecificMethods.java
+++ src/main/java/com/qa/ApplicationSpecifics/ProjectSpecificMethods.java
@@ -2,6 +2,8 @@ package com.qa.ApplicationSpecifics;
 import java.io.IOException;
 import java.util.Random;
 
+// Test- repo checkin 
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
